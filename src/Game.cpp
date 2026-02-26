887843b160f4bcf923808a459fab4034c843fbc0
RoxasMelvino <melvinoroxas@gmail.com>
2026-02-26 15:18:17 -0800

    create CreateBullet() method and allow player to shoot

--- src/Game.cpp
+++ src/Game.cpp
@@ -1,12 +1,34 @@
 #include "Game.h"
+#include <iostream>
+
+void Game::CreateBullet(Vector2 posInp, Vector2 dirInp, int speed) {
+    Bullet bullet(posInp, dirInp, speed);
+    bullets.push_back(bullet);
+}
 
 void Game::Update() {
     ClearBackground(BLACK);
+
     player.Update();
     enemy.Update();
+
+    Vector2 playerBulletDir;
+    playerBulletDir.x = 0;
+    playerBulletDir.y = -1; // player bullets will shoot straight up for now.
+
+    if (player.WantShoot()) { CreateBullet(player.GetPos(), playerBulletDir, 250); }
+
+    for (Bullet& bullet : bullets) {
+        // std::cout << "Updating bullet" << std::endl;
+        bullet.Update();
+    }
 }
 
 void Game::Draw() {
     player.Draw();
     enemy.Draw();
+    for (Bullet& bullet : bullets) {
+        bullet.Draw();
+    }
 }
+
--- src/Game.h
+++ src/Game.h
@@ -1,16 +1,21 @@
 #ifndef GAME_H
 #define GAME_H
 
+#include <vector>
 #include "entities/Player.h"
 #include "entities/Enemy.h"
+#include "projectiles/Bullet.h"
 
 class Game {
     private:
         Player player;
         Enemy enemy;
+        std::vector<Bullet> bullets;
+
     public:
         void Update();
         void Draw();
+        void CreateBullet(Vector2 posInp, Vector2 dirInp, int speed);
 };
 
 #endif //GAME_H
