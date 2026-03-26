4ef29a8e6f0c4eab0db62865ba9ddcd7a4b4ca7c
RoxasMelvino <melvinoroxas@gmail.com>
2026-03-26 10:50:22 -0700

    declare isTouchingBorder() method

--- src/entities/Enemy.cpp
+++ src/entities/Enemy.cpp
@@ -3,23 +3,33 @@
 Enemy::Enemy() {
     size = 25;
 
-     int startingPosX = GetScreenWidth() + size;
-     int startingPosY = GetScreenHeight() / 4;
+     int startingPosX = GetScreenWidth() - (size * 2);
+     int startingPosY = size * 2;
 
     pos.x = startingPosX;
     pos.y = startingPosY;
 
-    speed = 150; // hardcoded for now
+    speed = 300; // hardcoded for now
 }
 
 void Enemy::Update() {
-    pos.x -= speed * GetFrameTime(); // Enemies will only move left
+    pos.x += -speed * GetFrameTime(); // Enemies will only move left
 
     if (pos.x <= 0 - size) {
         pos.x = GetScreenWidth() + size;
+    }
+
+    if (IsTouchingBorder()) {
+        speed *= -1;
+        pos.y += (size / 2); // move down
     }
 }
 
 void Enemy::Draw() {
     DrawRectangle(pos.x, pos.y, size, size, RED); // they will be squares for now
 }
+
+bool Enemy::IsTouchingBorder() {
+    return (pos.x <= size || pos.x >= GetScreenWidth() - size);
+}
+
