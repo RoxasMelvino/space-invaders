c73d854e18981a70e94d3db0d691af064facbf41
RoxasMelvino <melvinoroxas@gmail.com>
2026-03-28 14:18:05 -0700

    create logic for enemy shooting cooldown and intervals

--- src/entities/Enemy.cpp
+++ src/entities/Enemy.cpp
@@ -9,19 +9,34 @@ Enemy::Enemy() {
     pos.x = startingPosX;
     pos.y = startingPosY;
 
-    speed = 300; // hardcoded for now
+    speed = 500;            // hardcoded for now
+    shootingCooldown = 2;   // 2 seconds cooldown initially for now
+    shootingInterval = 3;   // for shootingCooldown
+    timer = 0.0;            // to keep track of time for shootingCooldown
 }
 
 void Enemy::Update() {
-    pos.x += -speed * GetFrameTime(); // Enemies will only move left
+    float dt = GetFrameTime();
+    timer += dt;
+
+    pos.x += -speed * dt; // Enemies will only move left
 
     if (pos.x <= 0 - size) {
         pos.x = GetScreenWidth() + size;
     }
 
+    //TODO: Fix enemy moving down too much after hitting border
     if (IsTouchingBorder()) {
+        std::cout << "x: " << pos.x << " " << "y: " << pos.y << std::endl;
         speed *= -1;
         pos.y += (size / 2); // move down
+    }
+
+    wantShoot = false;
+    if (timer >= shootingCooldown) {
+        timer = 0;
+        wantShoot = true;
+        shootingCooldown = GetRandomValue(0, shootingInterval);
     }
 }
 
@@ -32,4 +47,13 @@ void Enemy::Draw() {
 bool Enemy::IsTouchingBorder() {
     return (pos.x <= size || pos.x >= GetScreenWidth() - size);
 }
+
+bool Enemy::WantShoot() {
+    return wantShoot;
+}
+
+Vector2 Enemy::GetPos() {
+    return pos;
+}
+
 
--- src/entities/Enemy.h
+++ src/entities/Enemy.h
@@ -2,18 +2,25 @@
 #define ENEMY_H
 
 #include "raylib.h"
+#include <iostream>
 
 class Enemy {
     private:
         Vector2 pos;
         double speed;
         int size; // enemies will vary in size later
+        float timer;
+        int shootingInterval;
+        int shootingCooldown;
+        bool wantShoot;
 
     public:
         Enemy();
         void Update();
         void Draw();
         bool IsTouchingBorder();
+        bool WantShoot();
+        Vector2 GetPos();
 };
 
 
